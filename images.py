b897f9357b4fccb28c2ecd2013dbbba58983b8d6
Michael Melesse <micmelesse@gmail.com>
2017-11-03 15:15:49 -0400

    create 4d input tensor

--- images.py
+++ images.py
@@ -1,10 +1,11 @@
 import os
 import sys
+import requests
 import tarfile
 import zipfile
 import numpy as np
-from scipy import ndimage
-import requests
+from PIL import Image
+
 from urllib.request import urlretrieve
 
 
@@ -14,20 +15,27 @@ def read_images(archive_link):
     print(archive_url.headers)
     sys.exit()
 
-
-def constuct_input_matrix():
-    im_path=
     cur_dir=os.listdir();
     if(im_path not in  cur_dir):
         if(im_arc not in  cur_dir):
-            tarfile.open(caltech_path+'.tar').extractall()
+            tarfile.open(im_path+'.tar').extractall()
 
-    for root, subdirs, files in os.walk(caltech_path):
+
+def constuct_input_matrix(im_dir="ImageNet"):
+    ret=[];
+    for root, subdirs, files in os.walk(im_dir):
         for f in files:
-            print(ndimage.imread(f))
-            sys.exit()
+            try:
+                im=np.array(Image.open(root+'/'+f));
+                if im.ndim is 3:
+                    im.resize((255,255,3))
+                    ret.append(im);
+            except OSError:
+                pass;
+
+    return np.stack(ret2)
 
 
-if __name__ == '__main__':
-    constuct_input_matrix()
+# if __name__ == '__main__':
+    #     constuct_input_matrix()
     #read_images('http://cs231n.stanford.edu/tiny-imagenet-200.zip')
