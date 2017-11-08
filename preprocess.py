6f325207fdabfb5e92211581f2a5b82391f0a65f
Michael Melesse <micmelesse@gmail.com>
2017-11-08 15:56:02 -0500

    net constructor portion

--- preprocess.py
+++ preprocess.py
@@ -3,18 +3,49 @@ import sys
 import random
 import trimesh
 import numpy as np
+import requests
+import tarfile
+import zipfile
+from PIL import Image
+from urllib.request import urlretrieve
 
-# default input is sysnet_id for airplanes
+
+def constuct_input_matrix(im_dir):
+    ret = []
+    for root, subdirs, files in os.walk(im_dir):
+        for f in files:
+            try:
+                im = Image.open(root + '/' + f)
+                im = im.resize((512, 512))
+                im = np.array(im)
+                if im.ndim is 3:
+                    # remove alpha channel
+                    ret.append(im[:, :, 0:3]);
+            except:
+                pass
+
+    return np.stack(ret)
+
+
+def extract_archives(archive_link):
+    archive_url = requests.get(archive_link, stream=True)
+    print(archive_url.headers)
+    sys.exit()
+
+    cur_dir = os.listdir()
+    if(im_path not in cur_dir):
+        if(im_arc not in cur_dir):
+            tarfile.open(im_path + '.tar').extractall()
 
 
 def generate_renders_from_mesh(mesh):
     print("[generate_images_from_mesh]")
+    os.system("rm -rf Renders/*")
     scene = mesh.scene()
-    N = random.randint(5, 20)
+    N = random.randint(5, 15)
     for i in range(N):
         angle = np.radians(random.randint(30, 60))
-        axis = [random.randint(0, 2), random.randint(
-            0, 2), random.randint(0, 2)]
+        axis = random.choice([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
         rotate = trimesh.transformations.rotation_matrix(
             angle, axis, scene.centroid)
         camera_old, _geometry = scene.graph['camera']
@@ -58,8 +89,7 @@ def main():
     else:
         compund_mesh = mesh_obj
 
-    meshes.append(compund_mesh)
-    generate_renders_from_mesh(meshes[0])
+    generate_renders_from_mesh(compund_mesh)
 
 
 if __name__ == '__main__':
