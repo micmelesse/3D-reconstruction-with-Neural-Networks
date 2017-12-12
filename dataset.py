d7078f5befa2576386b0f42cf9131c46c378cd31
Michael Melesse <micmelesse@gmail.com>
2017-12-12 16:05:51 -0500

    started using fork keras

--- dataset.py
+++ dataset.py
@@ -12,16 +12,17 @@ from PIL import Image
 def load_dataset(dataset_dir="./ShapeNet", N=None):
     print("[load_dataset] loading from {0}".format(dataset_dir))
 
+    ret = []
     pathlist_tuple = construct_paths(dataset_dir, file_types=['.obj', '.mtl'])
     pathlist = pathlist_tuple[0]
-    pathlist = pathlist[:N] if N is None else pathlist
+    pathlist = pathlist[:N] if N is not None else pathlist
     for mesh_path in pathlist:
         if not os.path.isfile(mesh_path):
             continue
         try:
             mesh_obj = trimesh.load_mesh(mesh_path)
         except Exception as e:
-            print(e)
+            print("failed to load {}".format(mesh_path))
             continue
 
         if isinstance(mesh_obj, list):
@@ -32,19 +33,22 @@ def load_dataset(dataset_dir="./ShapeNet", N=None):
             compund_mesh = mesh_obj
 
         render_dir = "./ShapeNet_Renders"
-        render_path = os.path.dirname(
+        render_path_tuple = os.path.split(
             str.replace(mesh_path, dataset_dir, render_dir))
 
-        if os.path.isdir(render_path) and os.listdir(render_path) != []:
-            return fetch_renders_from_disk(render_path)
+        if os.path.isdir(render_path_tuple[0]) and os.listdir(render_path_tuple[0]) != []:
+            ret.append(fetch_renders_from_disk(render_path_tuple[0]))
         else:
-            write_renders_to_disk(compund_mesh, render_path, 10)
-            return fetch_renders_from_disk(render_path)
+            write_renders_to_disk(compund_mesh, render_path_tuple, 10)
+            ret.append(fetch_renders_from_disk(render_path_tuple[0]))
+
+    return ret
 
 
-def write_renders_to_disk(mesh, render_path, N=5):
-    os.system("rm -rf {}".format(render_path))  # FIXME: stupid but clean
-    os.makedirs(render_path)
+def write_renders_to_disk(mesh, render_path_tuple, N=5):
+    # FIXME: stupid but clean
+    os.system("rm -rf {}".format(render_path_tuple[0]))
+    os.makedirs(render_path_tuple[0])
     scene = mesh.scene()
     for i in range(N):
         angle = np.radians(random.randint(30, 60))
@@ -54,8 +58,9 @@ def write_renders_to_disk(mesh, render_path, N=5):
         camera_old, _geometry = scene.graph['camera']
         camera_new = np.dot(camera_old, rotate)
         scene.graph['camera'] = camera_new
-        # pyglet.gl.glDisable(pyglet.gl.GL_CULL_FACE)
-        scene.save_image('{0}/render_{1}.png'.format(render_path, i))
+        # backfaces culled if using original trimesh package
+        scene.save_image(
+            '{0}/{1}_{2}.png'.format(render_path_tuple[0], os.path.splitext(render_path_tuple[1]), i))
 
 
 def fetch_renders_from_disk(render_path):
@@ -72,8 +77,6 @@ def fetch_renders_from_disk(render_path):
             except Exception as e:
                 print(e)
                 continue
-    # print("[fetch_renders_from_disk] returning {0} renders from {1}".format(
-    #     len(ret), render_path))
     return np.stack(ret)
 
 
@@ -90,4 +93,4 @@ def construct_paths(data_dir, file_types):
 
 
 if __name__ == '__main__':
-    load_dataset()
+    load_dataset(N=5)
