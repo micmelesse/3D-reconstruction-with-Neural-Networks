bbaed8c609784ce7f12c19c88f932276e7c7133f
Michael Melesse <micmelesse@gmail.com>
2017-12-16 15:44:15 -0500

    network end to end setup

--- open.sh
+++ open.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-jupyter notebook stop 8888 # this works in the latest version of jupyter
 jupyter notebook &> /dev/null &
 jupyter notebook list
--- tensorboard.sh
+++ tensorboard.sh
@@ -1,5 +1,5 @@
 #jupyter nbconvert --to notebook --execute main.ipynb
 pkill -f tensorboard
 tensorboard --host=localhost --logdir='./logs/' &> /dev/null &
-sleep 1
+sleep 4
 open http://localhost:6006
