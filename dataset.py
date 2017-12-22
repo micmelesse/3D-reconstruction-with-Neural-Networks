f9eef3cd7de2f826af4294cecf45d43ca9d27d89
Michael Melesse <micmelesse@gmail.com>
2017-12-21 22:45:56 -0500

    training runs without crashing

--- dataset.py
+++ dataset.py
@@ -6,55 +6,97 @@ import tarfile
 import pyglet
 import trimesh
 import binvox_rw
+import math
 import numpy as np
 import pandas as pd
 from PIL import Image
 from filecmp import dircmp
 
-def get_paths(data_dir="ShapeNetRendering", label_dir="ShapeNetVox32"):
 
-        common_paths = []
-        for dir_top, subdir_cmps in dircmp(data_dir, label_dir).subdirs.items():
-            for dir_bot in subdir_cmps.common_dirs:
-                common_paths.append(os.path.join(dir_top, dir_bot))
-
-        mapping = pd.DataFrame(common_paths, columns=["common_dirs"])
-        mapping['data_dirs'] = mapping.apply(
-            lambda row: os.path.join(data_dir, row.common_dirs), axis=1)
-
-        mapping['label_dirs'] = mapping.apply(
-            lambda row: os.path.join(label_dir, row.common_dirs), axis=1)
-
-        data_paths = []
-        label_paths = []
-        for d, l in zip(mapping.data_dirs, mapping.label_dirs):
-            png_ls = construct_path_lists(d, [".png"])
-            binvox_ls = construct_path_lists(l, [".binvox"])
-            data_paths += png_ls
-            label_paths += binvox_ls * len(png_ls)
-
-        paths = pd.DataFrame({"data_paths": data_paths, "label_paths": label_paths})
-        paths.to_csv("paths.csv")
-        return paths  
-
-def load_dataset(dataset_dir="ShapeNetRendering", num_of_examples=None):
-    return fetch_renders_from_disk(dataset_dir, num_of_examples)
+def main():
+    shapenet = ShapeNet()
+    train = shapenet.next_train_batch(25)
+    X = load_dataset(train[:, 0])
+    Y = load_labels(train[:, 1])
 
 
-def load_labels(dataset_dir="ShapeNetVox32", num_of_examples=None):
-    pathlist_tuple = construct_path_lists(
-        dataset_dir, file_types=['.binvox'])
-    pathlist = pathlist_tuple[0]
-    random.shuffle(pathlist)
-    pathlist = pathlist[:num_of_examples] if num_of_examples is not None else pathlist
+class ShapeNet:
+    def __init__(self):
+
+        self.paths = pd.DataFrame.from_csv("paths.csv").as_matrix()
+        np.random.shuffle(self.paths)
+        self.count = len(self.paths)
+        self.split_index = math.ceil(self.count * 0.8)
+        self.train_index = 0
+        self.test_index = self.split_index
+        self.batch_size = 32
+
+    def next_train_batch(self, batch_size=None):
+        if batch_size is None:
+            batch_size = self.batch_size
+
+        prev_index = self.train_index
+        self.train_index += batch_size
+        if prev_index >= self.split_index:
+            self.train_index = 0
+            return None
+        elif self.train_index >= self.split_index:
+
+            return self.paths[prev_index:self.split_index]
+        else:
+            return self.paths[prev_index:self.train_index]
+
+    def next_test_batch(self, batch_size=None):
+        if batch_size is None:
+            batch_size = self.batch_size
+
+        prev_index = self.test_index
+        self.test_index += batch_size
+
+        if prev_index >= self.count:
+            self.test_index = self.split_index
+            return None
+        elif self.test_index >= self.count:
+            return self.paths[prev_index:self.count]
+        else:
+            return self.paths[prev_index:self.test_index]
+
+    def reset():
+        np.random.shuffle(self.paths)
+        self.train_index = 0
+        self.test_index = split_index
+
+
+def load_dataset(dataset_path_list):
+    if isinstance(dataset_path_list, np.ndarray):
+        dataset_path_list = dataset_path_list.tolist()
+
+    return fetch_render_from_disk(dataset_path_list)
+
+
+def load_labels(label_path_list):
+    if isinstance(label_path_list, np.ndarray):
+        label_path_list = label_path_list.tolist()
+
     voxel_list = []
-
-    for voxel_path in pathlist:
+    for voxel_path in label_path_list:
         with open(voxel_path, 'rb') as f:
             voxel_list.append(
                 (binvox_rw.read_as_3d_array(f)).data.astype(float))
 
     return np.stack(voxel_list)
+
+
+def fetch_render_from_disk(render_path_list):
+    png_list = []
+    for png_file in render_path_list:
+        im = Image.open(png_file)
+        im = np.array(im)
+        if im.ndim is 3:
+            # remove alpha channel
+            png_list.append(im[:, :, 0:3])
+
+    return np.stack(png_list)
 
 
 def render_dataset(dataset_dir="ShapeNet", num_of_examples=None, render_count=24):
@@ -88,10 +130,10 @@ def render_dataset(dataset_dir="ShapeNet", num_of_examples=None, render_count=24
             str.replace(mesh_path, dataset_dir, render_dir))
 
         if os.path.isdir(render_path) and os.listdir(render_path) != []:
-            render_list.append(fetch_renders_from_disk(render_path))
+            render_list.append(fetch_render_from_disk(render_path))
         else:
             write_renders_to_disk(compund_mesh, render_path, render_count)
-            render_list.append(fetch_renders_from_disk(render_path))
+            render_list.append(fetch_render_from_disk(render_path))
 
     return render_list
 
@@ -118,33 +160,36 @@ def write_renders_to_disk(mesh, render_path, render_count=10):
     return
 
 
-def fetch_renders_from_disk(render_path="ShapeNetRendering", num_of_examples=None):
-    print("[fetch_renders_from_disk] fetching renders from {0} ... ".format(
-        render_path))
+def get_paths(data_dir="ShapeNetRendering", label_dir="ShapeNetVox32"):
 
-    pathlist_tuple = construct_path_lists(render_path, file_types=['.png'])
-    pathlist = pathlist_tuple[0]
-    random.shuffle(pathlist)
-    pathlist = pathlist[:num_of_examples] if num_of_examples is not None else pathlist
-    png_list = []
+    common_paths = []
+    for dir_top, subdir_cmps in dircmp(data_dir, label_dir).subdirs.items():
+        for dir_bot in subdir_cmps.common_dirs:
+            common_paths.append(os.path.join(dir_top, dir_bot))
 
-    for png_file in pathlist:
-        try:
-            im = Image.open(png_file)
-            im = np.array(im)
-            if im.ndim is 3:
-                # remove alpha channel
-                png_list.append(im[:, :, 0:3])
-        except Exception as e:
-            print("[fetch_renders_from_disk] failed on {0} ... ".format(
-                render_path))
-            continue
+    mapping = pd.DataFrame(common_paths, columns=["common_dirs"])
+    mapping['data_dirs'] = mapping.apply(
+        lambda row: os.path.join(data_dir, row.common_dirs), axis=1)
 
-    return np.stack(png_list)
+    mapping['label_dirs'] = mapping.apply(
+        lambda row: os.path.join(label_dir, row.common_dirs), axis=1)
+
+    data_paths = []
+    label_paths = []
+    for d, l in zip(mapping.data_dirs, mapping.label_dirs):
+        png_ls = construct_path_lists(d, [".png"])
+        binvox_ls = construct_path_lists(l, [".binvox"])
+        data_paths += png_ls
+        label_paths += binvox_ls * len(png_ls)
+
+    paths = pd.DataFrame(
+        {"data_paths": data_paths, "label_paths": label_paths})
+    paths.to_csv("paths.csv")
+    return paths
 
 
 def construct_path_lists(data_dir, file_types):
-    #print("[construct_path_lists] parsing dir {} for {} ...".format(data_dir, file_types))
+    # print("[construct_path_lists] parsing dir {} for {} ...".format(data_dir, file_types))
     paths = [[] for _ in range(len(file_types))]
 
     for root, _, files in os.walk(data_dir):
@@ -157,3 +202,41 @@ def construct_path_lists(data_dir, file_types):
         return paths[0]
 
     return tuple(paths)
+
+
+def download_dataset(dataset_name="ShapeNet"):
+    print("[download_dataset]")
+    if(dataset_name is "ShapeNet"):
+        f = urlopen(
+            "http://shapenet.cs.stanford.edu/shapenet/obj-zip/ShapeNetSem.v0/README.txt")
+
+        shapenet_urls = re.findall('https.*', (f.read()).decode("utf-8"))
+        shapenet_metadata = []
+        for s in shapenet_urls:
+            shapenet_metadata.append(pd.read_csv(s))
+
+        index_metadata = 0
+        id_list = (shapenet_metadata[index_metadata])["wnsynset"]
+        for i, id in enumerate(id_list):
+            id = 0
+            print(id)
+            download_link = "http://shapenet.cs.stanford.edu/shapenet/obj-zip/ShapeNetCore.v1/{}.zip".format(
+                id)
+            # urlretrieve(download_link, "./data/{}.zip" + id)
+            # sys.exit()
+
+
+def extract_archives(archive_link):
+    im_path, im_arc = None, None
+   # archive_url = requests.get(archive_link, stream=True)
+   # print(archive_url.headers)
+    sys.exit()
+
+    cur_dir = os.listdir()
+    if(im_path not in cur_dir):
+        if(im_arc not in cur_dir):
+            tarfile.open(im_path + '.tar').extractall()
+
+
+if __name__ == '__main__':
+    main()
