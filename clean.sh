f879801513d8d27ed9dd2b033e515936a16d97ff
Michael Melesse <micmelesse@gmail.com>
2018-01-06 01:16:11 -0500

    visualizing the data

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 rm -rf ./train_dir/*
-rm -rf ./out_dir/*
 rm -rf ./train_dir_aws/*
-rm -rf ./out_dir_aws/*
+rm -rf ./out_aws/*
 rm -rf ./logs/*
+rm -rf ./out/*
 rm -rf ./ShapeNet_Renders/*
 rm -f ./*.nbconvert.ipynb
 rm -f train.py
---
+++ voxel_show.sh
@@ -0,0 +1,1 @@
+./viewvox -ki out/*.binvox
