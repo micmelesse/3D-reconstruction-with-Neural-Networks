2d4e45a21b8f89d64e514f7e4362e51eaa400c30
Michael Melesse <micmelesse@gmail.com>
2018-01-15 12:26:13 -0500

    minor

--- scp_aws.sh
+++ scp_aws.sh
@@ -1,4 +1,5 @@
 source aws.params
-scp -ri $key ec2-user@$dns:thesis/logs/* ./aws_logs
-scp -ri $key ec2-user@$dns:thesis/train_dir/* ./aws_train
-scp -ri $key ec2-user@$dns:thesis/out/* ./aws_out
+# scp -ri $key ec2-user@$dns:thesis/logs/* ./aws_logs
+# scp -ri $key ec2-user@$dns:thesis/train_dir/* ./aws_train
+# scp -ri $key ec2-user@$dns:thesis/out/* ./aws_out
+scp -ri $key ec2-user@$dns:cos429_f17_final_project/model* ./aws_model
