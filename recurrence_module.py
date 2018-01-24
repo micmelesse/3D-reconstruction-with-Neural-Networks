5246eebd7b98678e3f4fe03ba8696d17d4859952
Michael Melesse <micmelesse@gmail.com>
2018-01-24 13:44:27 -0500

    stuff

--- recurrence_module.py
+++ recurrence_module.py
@@ -6,89 +6,7 @@ import tensorflow as tf
 import utils
 
 
-def main():
-    pass
-
-
-def bias_grid(name, N, n_h):
-    i_list = []
-    for i in range(N):
-        j_list = []
-        for j in range(N):
-            k_list = []
-            for k in range(N):
-                k_list.append(tf.Variable(
-                    tf.fill([n_h], 1 / math.sqrt(1024)), name="W{}_{}{}{}".format(name, i, j, k)))
-            j_list.append(k_list)
-        i_list.append(j_list)
-
-    return i_list
-
-
-def weight_grid(name, N, n_x, n_h):
-    i_list = []
-    for i in range(N):
-        j_list = []
-        for j in range(N):
-            k_list = []
-            for k in range(N):
-                k_list.append(tf.Variable(tf.fill(
-                    [n_x, n_h], 1 / math.sqrt(n_x)), name="W{}_{}{}{}".format(name, i, j, k)))
-            j_list.append(k_list)
-        i_list.append(j_list)
-
-    return i_list
-
-
-def weight_grid_multiply(x, W, N=4):
-    W = np.array(W)
-    i_list = []
-    for i in range(N):
-        j_list = []
-        for j in range(N):
-            k_list = []
-            for k in range(N):
-                k_list.append(tf.matmul(x, W[i, j, k]))
-            j_list.append(k_list)
-        i_list.append(j_list)
-    return tf.transpose(tf.convert_to_tensor(i_list), [3, 0, 1, 2, 4])
-
-
 class GRU_R2N2:
-    def __init__(self, n_cells=4, n_input=1024, n_hidden_state=256):
-        self.W_u = weight_grid('u', n_cells, n_input, n_hidden_state)
-        self.W_r = weight_grid('r', n_cells, n_input, n_hidden_state)
-        self.W_h = weight_grid('h', n_cells, n_input, n_hidden_state)
-
-        self.b_u = bias_grid('u', n_cells, n_hidden_state)
-        self.b_r = bias_grid('r', n_cells, n_hidden_state)
-        self.b_h = bias_grid('h', n_cells, n_hidden_state)
-
-        self.U_u = tf.Variable(tf.random_normal(
-            [3, 3, 3, n_hidden_state, n_hidden_state]), name="U_u")
-        self.U_r = tf.Variable(tf.random_normal(
-            [3, 3, 3, n_hidden_state, n_hidden_state]), name="U_r")
-        self.U_h = tf.Variable(tf.random_normal(
-            [3, 3, 3, n_hidden_state, n_hidden_state]), name="U_h")
-
-    def call(self, fc_input, prev_state):
-        def linear(x, W, U, h, b):
-            Wx = weight_grid_multiply(x, W)
-            Uh = tf.nn.conv3d(h, U, strides=[1, 1, 1, 1, 1], padding="SAME")
-            return Wx + Uh + tf.convert_to_tensor(b)
-
-        u_t = tf.sigmoid(
-            linear(fc_input, self.W_u, self.U_u, prev_state, self.b_u))
-        r_t = tf.sigmoid(
-            linear(fc_input, self.W_r, self.U_r, prev_state,  self.b_r))
-        h_t = tf.multiply(1 - u_t, prev_state) + tf.multiply(u_t, tf.tanh(
-            linear(fc_input, self.W_h, self.U_h, tf.multiply(r_t, prev_state), self.b_h)))
-
-        return h_t
-
-
-class GRU_R2N2_MD_Weight:
-
     def __init__(self, n_cells=4, n_hidden_state=256):
         N = n_cells
         h_n = n_hidden_state
@@ -166,17 +84,37 @@ class LSTM_R2N2:
         return h_t, tf.contrib.rnn.LSTMStateTuple(c_t, h_t)
 
 
-# from tensorflow github boards
-def unpool3D(value, name='unpool3D'):
-    with tf.name_scope(name) as scope:
-        sh = value.get_shape().as_list()
-        dim = len(sh[1: -1])
-        out = (tf.reshape(value, [-1] + sh[-dim:]))
-        for i in range(dim, 0, -1):
-            out = tf.concat([out, tf.zeros_like(out)], i)
-        out_size = [-1] + [s * 2 for s in sh[1:-1]] + [sh[-1]]
-        out = tf.reshape(out, out_size, name=scope)
-    return out
+class GRU_R2N2_2:
+    def __init__(self, n_cells=4, n_input=1024, n_hidden_state=256):
+        self.W_u = utils.weight_grid('u', n_cells, n_input, n_hidden_state)
+        self.W_r = utils.weight_grid('r', n_cells, n_input, n_hidden_state)
+        self.W_h = utils.weight_grid('h', n_cells, n_input, n_hidden_state)
+
+        self.b_u = utils.bias_grid('u', n_cells, n_hidden_state)
+        self.b_r = utils.bias_grid('r', n_cells, n_hidden_state)
+        self.b_h = utils.bias_grid('h', n_cells, n_hidden_state)
+
+        self.U_u = tf.Variable(tf.random_normal(
+            [3, 3, 3, n_hidden_state, n_hidden_state]), name="U_u")
+        self.U_r = tf.Variable(tf.random_normal(
+            [3, 3, 3, n_hidden_state, n_hidden_state]), name="U_r")
+        self.U_h = tf.Variable(tf.random_normal(
+            [3, 3, 3, n_hidden_state, n_hidden_state]), name="U_h")
+
+    def call(self, fc_input, prev_state):
+        def linear(x, W, U, h, b):
+            Wx = utils.weight_grid_multiply(x, W)
+            Uh = tf.nn.conv3d(h, U, strides=[1, 1, 1, 1, 1], padding="SAME")
+            return Wx + Uh + tf.convert_to_tensor(b)
+
+        u_t = tf.sigmoid(
+            linear(fc_input, self.W_u, self.U_u, prev_state, self.b_u))
+        r_t = tf.sigmoid(
+            linear(fc_input, self.W_r, self.U_r, prev_state,  self.b_r))
+        h_t = tf.multiply(1 - u_t, prev_state) + tf.multiply(u_t, tf.tanh(
+            linear(fc_input, self.W_h, self.U_h, tf.multiply(r_t, prev_state), self.b_h)))
+
+        return h_t
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -4,27 +4,28 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def main():
-    pass
 
 # print ("use %matplotlib inline if you want to display result in a notebook")
 def imsave_multichannel(im, f_name):
     return plt.imsave(f_name, (flatten_multichannel_image(im)))
 
 # print ("use %matplotlib inline if you want to display result in a notebook")
-def imshow_multichannel(im):  
+
+
+def imshow_multichannel(im):
     return plt.imshow(flatten_multichannel_image(im))
 
+
 def imshow_voxel(vox):
-    fig=plt.figure()
+    fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.voxels(vox,edgecolor='k')
+    ax.voxels(vox, edgecolor='k')
     ax.view_init(30, 30)
     return plt.imshow()
 
 
 def flatten_multichannel_image(im):
-    #print(im.shape)
+    # print(im.shape)
     n_channels = im.shape[-1]
     n_tile = math.ceil(math.sqrt(n_channels))
     rows = []
@@ -49,9 +50,6 @@ def flatten_multichannel_image(im):
         a = np.vstack((a, b))
     return a
 
-def grid3D(element, N=4):
-    return np.array([[[element for k in range(N)] for j in range(N)] for i in range(N)])
-
 
 # returns the neighboors of a cell including the cell
 def get_neighbors(grid, loc=(0, 0, 0), dist=1):
@@ -67,5 +65,63 @@ def get_neighbors(grid, loc=(0, 0, 0), dist=1):
     return (grid[i_min:i_max + 1, j_min:j_max + 1, k_min:k_max + 1]).flatten()
 
 
-if __name__ == '__main__':
-    main()
+def bias_grid(name, N, n_h):
+    i_list = []
+    for i in range(N):
+        j_list = []
+        for j in range(N):
+            k_list = []
+            for k in range(N):
+                k_list.append(tf.Variable(
+                    tf.fill([n_h], 1 / math.sqrt(1024)), name="W{}_{}{}{}".format(name, i, j, k)))
+            j_list.append(k_list)
+        i_list.append(j_list)
+
+    return i_list
+
+
+def weight_grid(name, N, n_x, n_h):
+    i_list = []
+    for i in range(N):
+        j_list = []
+        for j in range(N):
+            k_list = []
+            for k in range(N):
+                k_list.append(tf.Variable(tf.fill(
+                    [n_x, n_h], 1 / math.sqrt(n_x)), name="W{}_{}{}{}".format(name, i, j, k)))
+            j_list.append(k_list)
+        i_list.append(j_list)
+
+    return i_list
+
+
+def weight_grid_multiply(x, W, N=4):
+    W = np.array(W)
+    i_list = []
+    for i in range(N):
+        j_list = []
+        for j in range(N):
+            k_list = []
+            for k in range(N):
+                k_list.append(tf.matmul(x, W[i, j, k]))
+            j_list.append(k_list)
+        i_list.append(j_list)
+    return tf.transpose(tf.convert_to_tensor(i_list), [3, 0, 1, 2, 4])
+
+# from tensorflow github boards
+
+
+def unpool3D(value, name='unpool3D'):
+    with tf.name_scope(name) as scope:
+        sh = value.get_shape().as_list()
+        dim = len(sh[1: -1])
+        out = (tf.reshape(value, [-1] + sh[-dim:]))
+        for i in range(dim, 0, -1):
+            out = tf.concat([out, tf.zeros_like(out)], i)
+        out_size = [-1] + [s * 2 for s in sh[1:-1]] + [sh[-1]]
+        out = tf.reshape(out, out_size, name=scope)
+    return out
+
+
+def grid3D(element, N=4):
+    return np.array([[[element for k in range(N)] for j in range(N)] for i in range(N)])
