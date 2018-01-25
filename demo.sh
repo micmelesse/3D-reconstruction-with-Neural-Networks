06958ee95602605f4f959c68cff935bed5c484b3
Michael Melesse <micmelesse@gmail.com>
2018-01-24 23:15:58 -0500

    minor

--- demo.sh
+++
@@ -1,5 +0,0 @@
-git pull
-screen -ls
-sh convert_to_script.sh demo
-python main.py #$learning_rate $batch_size $num_of_epochs
-sh send_email.sh
---
+++ train.sh
@@ -0,0 +1,5 @@
+git pull
+screen -ls
+sh convert_to_script.sh demo
+python demo.py
+# sh send_email.sh
