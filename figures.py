7543d444aeaed8dd171d3cee1d007a1780df3a71
Michael Melesse <micmelesse@gmail.com>
2018-04-01 21:45:34 -0400

    start experiments

---
+++ figures.py
@@ -0,0 +1,5 @@
+import lib.utils as utils
+from lib.path import construct_path_lists
+
+
+print(construct_path_lists("aws","L:0.01"))
--- lib/utils.py
+++ lib/utils.py
@@ -5,11 +5,14 @@ import sys
 import math
 import shutil
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from skimage import exposure
 from PIL import Image
+
+from filecmp import dircmp
 
 
 def montage(packed_ims, axis):
@@ -48,7 +51,7 @@ def montage_sequence(im):
 
 
 def prep_dir():
-    TRAIN_DIRS = ["out", "data", "aws"]
+    TRAIN_DIRS = ["out", "data", "aws","results"]
     for d in TRAIN_DIRS:
         make_dir(d)
 
@@ -101,3 +104,47 @@ def hstack(a, b):
 
 def vstack(a, b):
     return np.vstack((a, b))
+
+
+def construct_path_lists(data_dir, file_filter):
+    if isinstance(file_filter, str):
+        file_filter = [file_filter]
+    paths = [[] for _ in range(len(file_filter))]
+
+    for root, _, files in os.walk(data_dir):
+        for f_name in files:
+            for i, f_substr in enumerate(file_filter):
+                if f_substr in f_name:
+                    (paths[i]).append(root + '/' + f_name)
+
+    if len(file_filter) == 1:
+        return paths[0]
+
+    return tuple(paths)
+
+
+def write_path_csv(data_dir, label_dir):
+    print("creating path csv for {} and {}".format(data_dir, label_dir))
+
+    common_paths = []
+    for dir_top, subdir_cmps in dircmp(data_dir, label_dir).subdirs.items():
+        for dir_bot in subdir_cmps.common_dirs:
+            common_paths.append(os.path.join(dir_top, dir_bot))
+
+    mapping = pd.DataFrame(common_paths, columns=["common_dirs"])
+    mapping['data_dirs'] = mapping.apply(
+        lambda data_row: os.path.join(data_dir, data_row.common_dirs), axis=1)
+
+    mapping['label_dirs'] = mapping.apply(
+        lambda data_row: os.path.join(label_dir, data_row.common_dirs), axis=1)
+
+    table = []
+    for n, d, l in zip(common_paths, mapping.data_dirs, mapping.label_dirs):
+        data_row = [os.path.dirname(n)+"_"+os.path.basename(n)]
+        data_row += construct_path_lists(d, [".png"])
+        data_row += construct_path_lists(l, [".binvox"])
+        table.append(data_row)
+
+    paths = pd.DataFrame(table)
+    paths.to_csv("out/paths.csv")
+    return paths
