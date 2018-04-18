279651bab95ef4025744674ee4c23f892f286da6
Michael Melesse <micmelesse@gmail.com>
2018-04-18 11:25:05 -0400

    delete download module

--- lib/dataset.py
+++ lib/dataset.py
@@ -157,6 +157,29 @@ def write_path_csv(data_dir, label_dir):
     return paths
 
 
+def download_dataset(link):
+    download_folder = os.path.splitext(os.path.basename(link))[0]
+    archive = download_folder + ".tgz"
+
+    if not os.path.isfile(archive):
+        os.system('wget -c {0}'.format(link))
+
+    os.system("tar -xvzf {0}".format(archive))
+    os.rename(download_folder, "data/{}".format(download_folder))
+    os.system("rm -f {0}".format(archive))
+
+
+def download():
+    LABEL_LINK = 'ftp://cs.stanford.edu/cs/cvgl/ShapeNetVox32.tgz'
+    DATA_LINK = "ftp://cs.stanford.edu/cs/cvgl/ShapeNetRendering.tgz"
+
+    if not os.path.isdir("data/ShapeNetVox32"):
+        download_dataset(LABEL_LINK)
+
+    if not os.path.isdir("data/ShapeNetRendering"):
+        download_dataset(DATA_LINK)
+
+
 def main():
     example_count = utils.read_params()['TRAIN_PARAMS']['SAMPLE_SIZE']
     if not os.path.isfile("out/paths.csv"):
