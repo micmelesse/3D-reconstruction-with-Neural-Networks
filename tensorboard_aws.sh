066ee86edc9bc6fbb7482126948dee47e87cf35c
Michael Melesse <micmelesse@gmail.com>
2018-04-20 14:26:12 -0400

    minor

--- tensorboard_aws.sh
+++ tensorboard_aws.sh
@@ -1,5 +1,4 @@
 source read_params.sh
 pwd=$(pwd)
 LOG_DIR=$(ls -td $pwd/aws/* | head -1)
-open http://localhost:6006/
 tensorboard --logdir=$LOG_DIR
--- tensorboard_out.sh
+++ tensorboard_out.sh
@@ -1,6 +1,5 @@
 source read_params.sh
 pwd=$(pwd)
 LOG_DIR=$(ls -td $pwd/out/* | head -1)
-open http://localhost:6006/
 tensorboard --logdir=$LOG_DIR
 
