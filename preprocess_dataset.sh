433508ca05fed24bb6859dad465f4e679034affc
Michael Melesse <micmelesse@gmail.com>
2018-04-29 21:56:37 -0400

    update setup scripts

--- preprocess_dataset.sh
+++ preprocess_dataset.sh
@@ -1,6 +1,5 @@
-sh setup_dir.sh
-sh download_dataset.sh
-sh clean_preprocessed_data.sh
 python -c "from lib import dataset
 if __name__ == '__main__':
+    dataset.setup_dir()
+    dataset.download_dataset()
     dataset.preprocess_dataset()"
